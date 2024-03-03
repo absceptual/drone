f291a5a19d65cda55cc53e87a5e56eab0293a272
absceptual <absceptual@gmail.com>
2024-03-02 19:30:40 -0600

    feat: util function to handle sent string data

--- client/headers/utility.h
+++ client/headers/utility.h
@@ -5,4 +5,5 @@ namespace utility
 {
     std::string base64_encode( std::uint8_t* buffer, size_t length );
     std::map<std::string, std::string> parse_header( const std::string& header );
+    std::string string_from_recv( std::vector<std::uint8_t> buffer );
 }
