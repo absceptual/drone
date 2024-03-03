7f7b011a8d1baefe1fbac92c13fe6e123d784213
absceptual <absceptual@gmail.com>
2024-03-02 20:09:34 -0600

    test: client->server communication

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,6 +2,7 @@
 #include "../headers/client.h"
 #include "../headers/socket.h"
 #include "../headers/utility.h"
+#include "../headers/packet.h"
 
 int main()
 {
@@ -21,7 +22,31 @@ int main()
     }
     
     std::printf( "[+] client initialized!\n" );
-    std::cin.get();
-    WSACleanup();
+    std::printf( "[+] client attempts to say hello server!\n" );
+
+    const char* data = "hello server!";
+    client.send( reinterpret_cast< const std::uint8_t* >( data ), strlen(data) );
+    std::printf( "[+] server responds and says %s\n", client.recv_string( ).c_str( ) );
+
+    std::printf( "[+] client sends the server something in binary..\n" );
+
+    unsigned char secret = 0x90;
+    client.send( reinterpret_cast< std::uint8_t* >( &secret ), sizeof( std::uint8_t ) );
+
+    auto response_string = client.recv_string( );
+    if ( response_string.empty() )
+    {
+        std::printf( "[+] server responded with nothing..." );
+        std::cin.get( );
+
+        return -3;
+    }
+
+    std::printf( "[+] server responds and says: 0x%s\n", response_string.c_str( ) );
+
+    auto response = client.recv( );
+    bool correct = *response.data( ) == secret;
+    std::printf( "[+] did the server get it right? %s\n", correct ? "yes!" : "no" );
+    std::cin.get( );
     return 0;
 }   
